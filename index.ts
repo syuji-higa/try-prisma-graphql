92e51032788f2c328599602e705fe7ba708ead93
syuji-higa <syuji-higa@gmail.com>
2021-01-05 00:40:35 +0900

    feat: add create method

--- index.ts
+++ index.ts
@@ -3,7 +3,26 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 async function main() {
-  // ... you will write your Prisma Client queries here
+  await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+      posts: {
+        create: { title: "Hello World" },
+      },
+      profile: {
+        create: { bio:  "I like turtles" }
+      }
+    }
+  })
+
+  const allUsers = await prisma.user.findMany({
+    include: { 
+      posts: true,
+      profile: true 
+    }
+  })
+  console.dir(allUsers, { depth: null })
 }
 
 main()
